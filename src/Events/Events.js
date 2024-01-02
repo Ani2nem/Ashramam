9f85d5b3f7f6517231bd765fed5e4649b338e9aa
Ani2nem <ani2nem@gmail.com>
2024-01-02 16:56:30 -0600

    working on events section, added calender and downloaded scheduler package

--- src/Events/Events.js
+++ src/Events/Events.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import './Events.css'
+import React from 'react';
+import './Events.css';
+import { Inject, ScheduleComponent, Day, Week, Month, Agenda} from '@syncfusion/ej2-react-schedule';
 
 const Events = () => {
-    return (
-        <section id="Events" className='events-section'>
-            <h1 className='events-text'>Events</h1>
-            <img className="incharge-image" src="" alt="incharge"/>
-        </section>
-    )
-}
+        return (
+            <section id="Events" className="events-section">
+                <h1 className="events-text">Events</h1>
+                <div className='scheduler'>
+                <ScheduleComponent currentView='Month' >
+                    <Inject services={[Day, Week, Month, Agenda]}/>
+                </ScheduleComponent>    
+                </div>
+                
+           </section>
+        );
+    }
 
 export default Events;
