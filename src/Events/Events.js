214e5e1181b6dd0f6ac4b63e679d167107b639e6
Ani2nem <ani2nem@gmail.com>
2024-05-14 16:21:07 -0500

    removed syncfusion and events calender got wiped clean

--- src/Events/Events.js
+++ src/Events/Events.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './Events.css';
-import { Inject, ScheduleComponent, Day, Week, Month, Agenda} from '@syncfusion/ej2-react-schedule';
 
 const Events = () => {
         return (
             <section id="Events" className="events-section">
                 <h1 className="events-text">Events</h1>
                 <div className='scheduler'>
-                <ScheduleComponent currentView='Month' >
+                {/* <ScheduleComponent currentView='Month' >
                     <Inject services={[Day, Week, Month, Agenda]}/>
-                </ScheduleComponent>    
+                </ScheduleComponent>     */}
                 </div>
                 
            </section>
