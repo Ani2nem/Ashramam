0a23598e4abdd161756a53c5011cf0abcd52278b
Ani2nem <ani2nem@gmail.com>
2024-05-14 17:23:24 -0500

    fixed the about section to make it mobile responsive

--- src/About/About.js
+++ src/About/About.js
@@ -7,7 +7,8 @@ const About = () => {
             <h1 className='about-title'>About Ashramam</h1>
             <p className='about-text'>This ashramam was founded by and this sis wherlnlakfn sdlkfasjd nklfnasdlkf nlksdjflksd nflkasdjflnksdnflkjajdsfl;knsdf
                 adsf ldsa; kfjadslk nflkasdjfmksdnf ml;ksjdfoiedsfkldalsjflk lkasjdfl;dsjfl jfklmsd lfjlkdsaflknsafm asldkj ldsjflknsdf
-                lsjafka dsf;lasdjkflksajdfl ajsdf kn lkj lfjs adlkfjl asdflkjlkjdsfl lashlkvjjs alkjf asklfjl alj;lajf  lklahsjf asldfj al;
+                lsjafka dsf;lasdjkflksajdfl ajsdf kn lkj lfjs adlkfjl asdflkjlkjdsfl 
+                lashlkvjjs alkjf asklfjl alj;lajf  lklahsjf asldfj al;
                 aboutdklfjdfaksdfjs l adlskfj adfnclsdjkf oweijlknf salkjf;agjlkahg;fldaksjmf lasd fjaslfj alsfj lajdfoiwelshiofeh 
                 akfjhdas;l fjaosidjfel a;ahgkfajshdfo; ewno'jenwknri sofehgirhe sjtnfreia lajfljdaslj adlfjdlkj alkd a adkfjheiohakl
                 askfjndkl a;lkdjf
