c4c53796d799949443341511dc92f6bac67d368f
Ani2nem <ani2nem@gmail.com>
2024-05-14 17:36:50 -0500

    made Donate section responsive to mobile

--- src/Donate/Donate.js
+++ src/Donate/Donate.js
@@ -4,7 +4,7 @@ import './Donate.css'
 const Donate = () =>{
     return (
         <section className='donate-section'>
-            <h1 id="Donate" className='donation-title'>Contribute to Our Sai Baba</h1>
+            <h1 id="Donate" className='donation-title'>Contribute to Our Sai Baba Ashramam</h1>
             <button className='donate-button'>Donate</button>
             <p className='donation-message'>Your support not only fuels the growth of our temple but also enriches the 
                 spirit of our community, fostering unity, spiritual well-being, and communal harmony.
