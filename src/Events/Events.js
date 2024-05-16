1e6dbf46f50da410b0895d9255a202b0d3b82886
Ani2nem <ani2nem@gmail.com>
2024-05-16 14:29:21 -0500

    added react calender to the event section and udjusted the position of other elements to make app uniform

--- src/Events/Events.js
+++ src/Events/Events.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import './Events.css';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import './Events.css'
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
 
 const Events = () => {
-        return (
-            <section id="Events" className="events-section">
-                <h1 className="events-text">Events</h1>
-                <div className='scheduler'>
-                {/* <ScheduleComponent currentView='Month' >
-                    <Inject services={[Day, Week, Month, Agenda]}/>
-                </ScheduleComponent>     */}
-                </div>
-                
-           </section>
-        );
-    }
+  const events = [
+    { start: new Date(), end: new Date(), title: 'Post video' },
+    { start: new Date(), end: new Date(), title: 'Edit video' },
+    { start: new Date(), end: new Date(), title: 'Code' },
+  ];
+
+  return (
+    <section id="Events" className="events-section">
+      <h1 className="events-text">Events</h1>
+      <div className="calendar-js">
+        <Calendar
+          localizer={localizer}
+          events={events}
+          views={['month']} // Show only the month view
+          startAccessor="start"
+          endAccessor="end"
+        />
+      </div>
+    </section>
+  );
+};
 
 export default Events;
