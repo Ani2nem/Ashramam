a8c2016b4527503b60e1806c007e4c9d9ffd8b5e
Ani2nem <ani2nem@gmail.com>
2024-05-18 10:47:25 -0500

    added on click feature to the front end for the donate button

--- src/Donate/Donate.js
+++ src/Donate/Donate.js
@@ -2,10 +2,14 @@ import React from 'react'
 import './Donate.css'
 
 const Donate = () =>{
+    const DonateClicked = () => {
+        console.log("Donate button clicked!");
+    }
+    
     return (
         <section className='donate-section'>
             <h1 id="Donate" className='donation-title'>Contribute to Our Sai Baba Ashramam</h1>
-            <button className='donate-button'>Donate</button>
+            <button className='donate-button' onClick={DonateClicked}>Donate</button>
             <p className='donation-message'>Your support not only fuels the growth of our temple but also enriches the 
                 spirit of our community, fostering unity, spiritual well-being, and communal harmony.
                  Thank you for being a vital part of our noble cause.</p>
